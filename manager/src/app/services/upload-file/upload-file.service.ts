6074997a7d05851d46af6b18c612c393f8366e44
Manoj Saxena <manoj.saxena@gomotive.com>
2022-10-11 11:32:25 -0600

    upload service that works with upload component

---
+++ manager/src/app/services/upload-file/upload-file.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpEvent, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+
+  private baseUrl = 'http://localhost:8082';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+
+    const formData: FormData = new FormData();
+    formData.append('file', file)
+    const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
+      reportProgress: false,
+      responseType: 'json'
+    });
+
+    return this.http.request(req)
+  }
+}
