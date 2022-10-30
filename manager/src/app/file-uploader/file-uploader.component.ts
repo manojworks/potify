e3819827fa71fc460d49bf8f55790a2d032a9a30
Manoj Saxena <manoj.saxena@gomotive.com>
2022-10-29 18:36:32 -0600

    get specific response when file is uploaded

--- manager/src/app/file-uploader/file-uploader.component.ts
+++ manager/src/app/file-uploader/file-uploader.component.ts
@@ -1,15 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {UploadFileService} from "src/app/services/upload-file/upload-file.service"
 
+export interface IUploadResponse {
+  original_file_name: string,
+  saved_path: string,
+  errors: string
+}
+
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.css']
 })
+
 export class FileUploaderComponent implements OnInit {
 
   selectedFiles: FileList | null | undefined;
-  // private subscription: Subscription | undefined
+
+  private uploadResult: IUploadResponse | undefined
 
   constructor(private uploadService: UploadFileService) { }
 
@@ -18,11 +26,13 @@ export class FileUploaderComponent implements OnInit {
 
   fileUpload() {
     if (this.selectedFiles) {
-      console.log('num files ' + this.selectedFiles.length)
       for (var ind = 0; ind < this.selectedFiles.length; ind++) {
         console.log(this.selectedFiles[ind])
-        this.uploadService.upload(this.selectedFiles[ind]).subscribe(
-
+        this.uploadService.upload(this.selectedFiles[ind]).subscribe( resp => {
+            this.uploadResult = <IUploadResponse>resp
+            console.log("path is " + this.uploadResult.saved_path)
+            console.log(" original fule is " + this.uploadResult.original_file_name)
+          }
         )
       }
     }
