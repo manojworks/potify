216c4fc5ed64ed859135fb4751581469e27f7293
manojworks <manojworks@gmail.com>
2022-12-04 18:33:41 -0600

    include listings include cleaner upload url

--- song_service/song_server/urls.py
+++ song_service/song_server/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from song_upload import urls as song_upload_url
+from song_listings import urls as song_listings_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('song-upload/', include(song_upload_url)),
+    path('upload/', include(song_upload_url)),
+    path('listings/', include(song_listings_url)),
 ]
